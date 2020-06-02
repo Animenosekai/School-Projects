6ef8224835e3b8410d097bc5ac4457d37fbeb373
Animenosekai <maxence.osawa@gmail.com>
2020-06-03 00:46:35 +0200

    [add] adding comments

--- Python/Physics/etoile-polaire/data-etoile-polaire.py
+++ Python/Physics/etoile-polaire/data-etoile-polaire.py
@@ -1,3 +1,15 @@
+'''
+Explication du programme:
+- J'ai d'abord converti toutes les virgules des données en points dans les fichiers data-lambda-etoile-polaire.txt et data-intensite-etoile-polaire.txt
+- Je vérifie que les installations des modules ont bien été faites (ce que vous voyez juste en bas avec # IMPORTS)
+- J'ouvre chaque fichier (avec Python) pour pouvoir faire une boucle qui va lire chaque ligne des fichiers et ajouté chaque ligne à une liste. (le [:-1] est là pour enlever le saut de ligne qui est considéré comme un caractère dans python)
+- A l'aide de matplotlib, je trace les données que j'ai obtenu (les listes de données)
+
+lifeeasy est une librairie que j'ai codé moi-même et qui me sert pour raccourcir mes codes.
+
+© Anime no Sekai - 2020
+'''
+
 from subprocess import check_output # Using this import just to install the dependencies if not.
 
 # IMPORTS
@@ -24,7 +36,7 @@ line = file.readline()
 # Initialize counter for line number
 line_no = 1
 
-# Loop until EOF
+# Loop until EOF (fin)
 while line != '' :
     lambda_data.append(float(line[:-1]))
     line = file.readline()  
@@ -44,7 +56,7 @@ line = file.readline()
 # Initialize counter for line number
 line_no = 1
 
-# Loop until EOF
+# Loop until EOF (fin)
 while line != '' :
     intensite_data.append(float(line[:-1]))
     line = file.readline()  
@@ -53,8 +65,13 @@ while line != '' :
 # Close the file
 file.close()
 
-lifeeasy.clear()
-lifeeasy.display_action('Plotting')
+
+
+#### JE TRACE
+
+lifeeasy.clear() # JE NETTOIE LA CONSOLE
+lifeeasy.display_action('Plotting') # J'ANNONCE QUE JE TRACE A L'UTILISATEUR AVEC MA LIBRAIRIE
+
 plt.ylabel('Intensité')
 plt.xlabel('Wavelength, in nm')
 plt.title("Données de l'étoile polaire - Activité 3 p.274")
